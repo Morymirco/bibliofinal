3f0b8e758f7e1821d951bcb8e1e57d4e6b492d3e
Morymirco <morykoulibaly996@gmail.com>
2024-04-16 13:14:33 +0000

    bon ajouter
    ddd

--- src/similaire.jsx
+++ src/similaire.jsx
@@ -5,7 +5,7 @@ export default function Similaire() {
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
               <img
-                src="./img6.jpeg"
+                src="https://www.bod.fr/fileadmin/_processed_/2/0/csm_FR-easyCover_Roman-modern-1_d65ba05c25.jpg"
                 alt="Coffee"
                 class="w-full h-64 object-cover"
               />
@@ -27,7 +27,7 @@ export default function Similaire() {
     </div>
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
               <img
-                src="./img6.jpeg"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCkQPHrj-Ka9mkqKrUqK0KwYma84l7wNw5TpFCZ-RYzkEaa3ObdqDhldmqcDnDFgYNG_E&usqp=CAU"
                 alt="Coffee"
                 class="w-full h-64 object-cover"
               />
@@ -49,7 +49,7 @@ export default function Similaire() {
     </div>
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
               <img
-                src="./img6.jpeg"
+                src="https://i.pinimg.com/1200x/c7/1e/a1/c71ea17267cb76ede9cf664836b55828.jpg"
                 alt="Coffee"
                 class="w-full h-64 object-cover"
               />
